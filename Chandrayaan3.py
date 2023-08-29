f6f49b5ee46c474c83971699e4042d57efa1f807
gajjarmeshw <gajjarmeshw@gmail.com>
2023-08-29 20:35:41 +0530

    backward

--- Chandrayaan3.py
+++ Chandrayaan3.py
@@ -16,3 +16,19 @@ class Chandrayaan3:
             self.x += 1
         elif self.initial_direction == "W":
             self.x -= 1    
+
+    def backward_movement(self):
+            if self.initial_direction == "N":
+                self.y -= 1
+            elif self.initial_direction == "S":
+                self.y += 1
+            elif self.initial_direction == "E":
+                self.x -= 1
+            elif self.initial_direction == "W":
+                self.x += 1
+
+
+initial_position = (0, 0, 0)
+initial_direction = "N"
+chandrayaan3_obj = Chandrayaan3(initial_position[0],initial_position[1],initial_position[2], initial_direction)
+ 
