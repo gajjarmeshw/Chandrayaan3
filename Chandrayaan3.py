e9674d1425bcaba54c20423b56002f22b615648a
gajjarmeshw <gajjarmeshw@gmail.com>
2023-08-29 20:31:02 +0530

    forward movement

--- Chandrayaan3.py
+++ Chandrayaan3.py
@@ -5,3 +5,14 @@ class Chandrayaan3:
         self.y = y
         self.z = z
         self.initial_direction = initial_direction
+
+
+    def forward_movement(self):
+        if self.initial_direction == "N":
+            self.y += 1
+        elif self.initial_direction == "S":
+            self.y -= 1
+        elif self.initial_direction == "E":
+            self.x += 1
+        elif self.initial_direction == "W":
+            self.x -= 1    
